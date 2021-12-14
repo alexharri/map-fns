e490dee3d55bd5b8a7a93a28c7ade74b9a6515b3
alexharri <alexharri2919@gmail.com>
2021-12-14 20:28:15 +0000

    update comment

--- src/mergeInMap.ts
+++ src/mergeInMap.ts
@@ -2,8 +2,14 @@ import { AnyMap, MapOf } from "./types";
 
 /**
  * Creates a new `map` populated with every key in the original `map` where the
- * value behind each `k` in `keys` is the value `map[k]` merged with the return
- * value of `fn(map[k])`
+ * value behind each `k` in `keys` is the value `map[k]` shallowly merged with
+ * the return value of `fn(map[k])`
+ *
+ * ```tsx
+ *  newMap[k] = { ...map[k], ...fn(map[k]) };
+ * ```
+ *
+ * Example usage:
  *
  * ```tsx
  *  const out = modifyInMap(
@@ -20,7 +26,7 @@ import { AnyMap, MapOf } from "./types";
  * An error is thrown if any key in `keys` does not exist in the map.
  *
  * @param map - The map to copy and modify.
- * @param keys - The keys to modify within the map.
+ * @param keys - The keys to merge within the map.
  * @returns A new map with modified values.
  */
 export default function mergeInMap<

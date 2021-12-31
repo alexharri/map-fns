a241ec57b4580272758b63fae19fa994ebb00e39
alexharri <alexharri2919@gmail.com>
2021-12-31 14:35:29 +0000

    build(mapMap): include mapMap in the build output

--- src/index.ts
+++ src/index.ts
@@ -2,3 +2,4 @@ export { default as removeKeysFromMap } from "./removeKeysFromMap";
 export { default as modifyInMap } from "./modifyInMap";
 export { default as mergeInMap } from "./mergeInMap";
 export { default as addListToMap } from "./addListToMap";
+export { default as mapMap } from "./mapMap";

b73d8a3e2a6996c1bbc16574ba8e2383fbe55fb3
alexharri <alexharri2919@gmail.com>
2021-12-31 14:34:52 +0000

    feat(mapMap): add mapMap function

---
+++ src/mapMap.ts
@@ -0,0 +1,23 @@
+import { AnyMap, ItemInMap } from "./types";
+
+/**
+ * Creates a new `map` populated with every key in the original `map` where the
+ * value behind each `k` in `keys` is the return value of `fn(map[k])`.
+ *
+ * @param map The original map.
+ * @param fn A function that takes a single argument `map[k]` and returns the value of `newMap[k]`.
+ * @returns A new `map` where `map[k]` is the result of `fn(map[k])`.
+ */
+export default function mapMap<M extends AnyMap, T>(
+  map: M,
+  fn: (item: ItemInMap<M>) => T
+) {
+  const obj = {} as Record<keyof M, T>;
+  const keys = Object.keys(map) as Array<keyof M>;
+
+  for (const key in keys) {
+    obj[key] = fn(map[key]);
+  }
+
+  return obj;
+}

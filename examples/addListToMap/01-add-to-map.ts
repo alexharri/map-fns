8ab2bbbfaed655323604806a35cfeb8a7bacc7cc
alexharri <alexharri2919@gmail.com>
2022-01-10 21:36:37 +0000

    fix: add examples for addListToMap

---
+++ examples/addListToMap/01-add-to-map.ts
@@ -0,0 +1,16 @@
+import { addListToMap } from "map-fns";
+
+const map = {
+  a: { id: "a", value: 1 },
+  b: { id: "b", value: 2 },
+  c: { id: "c", value: 3 },
+};
+
+addListToMap(map, [{ id: "d", value: 4 }], "id");
+
+//=> {
+//     a: { id: "a", value: 1 },
+//     b: { id: "b", value: 2 },
+//     c: { id: "c", value: 3 },
+//     d: { id: "d", value: 4 },
+//   }

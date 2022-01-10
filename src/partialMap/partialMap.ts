4593df0d766f7d9be3fb575cc656c3d59b91ddcf
alexharri <alexharri2919@gmail.com>
2022-01-10 21:50:09 +0000

    feat(partialMap): add partialMap function

---
+++ src/partialMap/partialMap.ts
@@ -0,0 +1,34 @@
+import { AnyMap } from "../types";
+
+/**
+ * Creates a new map populated with every key in `keys` where the value
+ * behind each `k` in `keys` is `map[k]`.
+ *
+ * ```tsx
+ * partialMap({ a: 1, b: 2, c: 3 }, ["a", "c"]);
+ * //=> {
+ * //     a: 1,
+ * //     b: 3,
+ * //   }
+ * ```
+ *
+ * @param map The original map.
+ * @param keys The keys to include in the new map.
+ * @returns a new `map` populated with every key in `keys` where the value
+ * behind each `k` in `keys` is `map[k]`.
+ */
+export default function partialMap<M extends AnyMap>(
+  map: M,
+  keys: Array<keyof M>
+): M {
+  const obj = {} as M;
+
+  for (const key of keys) {
+    if (!map.hasOwnProperty(key)) {
+      throw new Error(`Key '${key}' does not exist in map.`);
+    }
+    obj[key] = map[key];
+  }
+
+  return obj;
+}
